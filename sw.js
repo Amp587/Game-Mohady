610fdfad33ba7f992d4ee183b7bb266fec8277eb
Amp587 <mhadiamp@gmail.com>
2026-07-02 18:54:53 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+javascriptconst CACHE = "tts-mohady-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json", 
+  "./icon-192.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
